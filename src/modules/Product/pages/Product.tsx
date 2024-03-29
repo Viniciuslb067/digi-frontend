4f83a85d1e337f9ff8ec5011748c1a05a7d9400b
Viniciuslb067 <viniciuslb067@gmail.com>
2024-03-29 16:39:09 -0300

    feat: add quantity input and cart functionality to Product page

--- src/modules/Product/pages/Product.tsx
+++ src/modules/Product/pages/Product.tsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
-import useGetProductBySlug from "@/services/Product/queries/useGetProductBySlug";
+import toast from "react-hot-toast";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useCart } from "@/context/CartProvider/CardProvider";
+import { Product as ProductEntity } from "@/entities/Product";
+import useGetProductBySlug from "@/services/Product/queries/useGetProductBySlug";
 
 const Product = () => {
   const { slug } = useParams<{ slug: string }>();
-
   const { data: response } = useGetProductBySlug({ slug });
 
+  const [quantity, setQuantity] = useState(1);
+
+  const { addToCart } = useCart();
+
+  const handleAddToCart = (product?: ProductEntity) => {
+    if (!product) return;
+
+    if (!quantity) {
+      toast.error("É necessário adicionar uma quantidade.");
+      return;
+    }
+
+    addToCart({ ...product, quantity });
+    setQuantity(1);
+
+    toast.success("Produto adicionado ao carrinho.");
+  };
+
   return (
-    <div className="p-32">
-      <div className="grid grid-cols-[1.25fr_1fr] gap-28">
+    <div className="">
+      <div className="grid grid-cols-1 lg:grid-cols-[1.25fr_1fr] gap-6 lg:gap-28">
         <div>
           <img
             className="w-full object-cover max-h-96"
@@ -26,14 +48,18 @@ const Product = () => {
             {response?.data?.price && `R$ ${response?.data?.price}`}
           </span>
           <div className="w-full flex flex-row gap-2">
-            <Button className="w-full text-md flex-1" size="lg">
+            <Button
+              className="w-full text-sm lg:text-md flex-1"
+              onClick={() => handleAddToCart(response?.data)}
+            >
               Adicionar ao Carrinho
             </Button>
             <Input
-              className="w-16 h-full"
-              type="number"
               min="1"
+              type="number"
               defaultValue="1"
+              className="w-14 lg:w-16 h-full"
+              onChange={(e) => setQuantity(Number(e.target.value))}
             />
           </div>
         </div>
