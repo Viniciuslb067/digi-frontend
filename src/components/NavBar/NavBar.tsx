4866417cb79780c042b8025706e4bf08d8011975
Viniciuslb067 <viniciuslb067@gmail.com>
2024-03-30 13:41:44 -0300

    refactor: refactor NavBar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,32 +1,21 @@
-import { ShoppingCart } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
-import { Button } from "../ui/button";
-import { useCart } from "@/context/CartProvider/CardProvider";
+import { CartButton, MyAccountMenu } from "./components";
 
 export const NavBar = () => {
   const navigate = useNavigate();
-  const { cart, setCartDrawerOpen } = useCart();
 
   return (
-    <div className="sticky z-50 inset-x-0 top-0 flex items-center justify-between bg-white px-6 py-6 lg:px-32">
+    <div className="sticky z-50 inset-x-0 top-0 flex items-center justify-between bg-white px-4 py-6 lg:px-32">
       <img
         onClick={() => navigate("/")}
-        className="w-32 h-14 cursor-pointer"
+        className="w-20 h-9 lg:w-32 lg:h-14 cursor-pointer"
         src="https://www.digi.ag/hs-fs/hubfs/Horizontal_Vermelho-8.png?width=200&height=94&name=Horizontal_Vermelho-8.png"
       />
-      <Button
-        size="icon"
-        className="bg-transparent hover:bg-transparent"
-        onClick={() => setCartDrawerOpen((prev) => !prev)}
-      >
-        <div className="flex flex-row gap-3 justify-center items-center">
-          <ShoppingCart color="black" />
-          <div className="size-5 bg-black rounded-full px-1 text-sm">
-            {cart.length}
-          </div>
-        </div>
-      </Button>
+      <div className="flex flex-row gap-4">
+        <MyAccountMenu />
+        <CartButton />
+      </div>
     </div>
   );
 };
