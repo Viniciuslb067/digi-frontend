dd44a05cd2bc4c14926112f9c9eaf044bb9f3c02
Viniciuslb067 <viniciuslb067@gmail.com>
2024-03-30 13:42:45 -0300

    feat: add CartButton and MyAccountMenu components

---
+++ src/components/NavBar/components/CartButton/CartButton.tsx
@@ -0,0 +1,22 @@
+import { ShoppingCart } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useCart } from "@/context/CartProvider/CardProvider";
+
+export const CartButton = () => {
+  const { cart, setCartDrawerOpen } = useCart();
+
+  return (
+    <Button
+      size="icon"
+      className="bg-transparent hover:bg-transparent"
+      onClick={() => setCartDrawerOpen((prev) => !prev)}
+    >
+      <div className="flex flex-row gap-1 lg:gap-2 justify-center items-center">
+        <ShoppingCart color="black" />
+        <div className="size-4 lg:size-5 bg-black rounded-full px-1 text-xs lg:text-sm">
+          {cart.length}
+        </div>
+      </div>
+    </Button>
+  );
+};
