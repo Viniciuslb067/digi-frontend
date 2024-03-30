9ac768db7cd630e2c692734c1f45aaaaba53180f
Viniciuslb067 <viniciuslb067@gmail.com>
2024-03-30 14:04:04 -0300

    fix: fix state null check in HistoryBreadcrumb.tsx

--- src/modules/Purchase/components/HistoryBreadcrumb/HistoryBreadcrumb.tsx
+++ src/modules/Purchase/components/HistoryBreadcrumb/HistoryBreadcrumb.tsx
@@ -13,7 +13,7 @@ export const HistoryBreadcrumb = () => {
   const { state } = useLocation();
 
   const renderFromUrl = () => {
-    if (state.includes("/product")) {
+    if (state?.includes("/product")) {
       const productName = state
         .split("/")
         .pop()
