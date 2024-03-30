4e188d1a0563df2c9103a90f102213f1b5e2e6af
Viniciuslb067 <viniciuslb067@gmail.com>
2024-03-30 13:44:40 -0300

    feat: add HistoryHeader, ProductItem, and History components

---
+++ src/modules/Purchase/components/HistoryHeader/HistoryHeader.tsx
@@ -0,0 +1,11 @@
+export const HistoryHeader = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Histórico de compras</h1>
+      <h3 className="text-gray-500">
+        Verifique o status de pedidos recentes, gerencie devoluções e descubra
+        produtos similares.
+      </h3>
+    </div>
+  );
+};
