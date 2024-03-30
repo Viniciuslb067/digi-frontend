0b3daa40331880cfb5744c5e41e61e80658e651b
Viniciuslb067 <viniciuslb067@gmail.com>
2024-03-30 14:03:34 -0300

    refactor: refactor purchase history rendering

--- src/modules/Purchase/pages/History.tsx
+++ src/modules/Purchase/pages/History.tsx
@@ -11,28 +11,39 @@ import { HistoryBreadcrumb, HistoryHeader, ProductItem } from "../components";
 const History = () => {
   const { data: response } = useGetPurchases();
 
+  const renderPurchases = () => {
+    if (!response?.data?.length)
+      return (
+        <div className="w-full mt-36 flex items-center justify-center">
+          <h1 className="text-xl">Não há compras realizadas.</h1>
+        </div>
+      );
+
+    return response?.data?.map((item) => (
+      <AccordionItem key={item.id} value={item.id.toString()}>
+        <AccordionTrigger>
+          <div className="flex flex-row gap-4 items-center justify-center">
+            <h3 className="text-sm lg:text-md">Compra #{item.id}</h3>
+            <h6 className="text-gray-400 font-light text-sm lg:text-md">
+              {formatDate(item.createdAt)}
+            </h6>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent>
+          {item?.products?.map((product) => (
+            <ProductItem key={product.slug} product={product} />
+          ))}
+        </AccordionContent>
+      </AccordionItem>
+    ));
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <HistoryBreadcrumb />
       <HistoryHeader />
       <Accordion type="single" collapsible className="w-full">
-        {response?.data?.map((item) => (
-          <AccordionItem key={item.id} value={item.id.toString()}>
-            <AccordionTrigger>
-              <div className="flex flex-row gap-4 items-center justify-center">
-                <h3 className="text-sm lg:text-md">Compra #{item.id}</h3>
-                <h6 className="text-gray-400 font-light text-sm lg:text-md">
-                  {formatDate(item.createdAt)}
-                </h6>
-              </div>
-            </AccordionTrigger>
-            <AccordionContent>
-              {item?.products?.map((product) => (
-                <ProductItem key={product.slug} product={product} />
-              ))}
-            </AccordionContent>
-          </AccordionItem>
-        ))}
+        {renderPurchases()}
       </Accordion>
     </div>
   );
