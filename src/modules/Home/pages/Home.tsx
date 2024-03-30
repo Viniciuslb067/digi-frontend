be5d7fa9bf7835d34cef0679deb63dd7bdd60f3c
Viniciuslb067 <viniciuslb067@gmail.com>
2024-03-30 14:03:58 -0300

    refactor: refactor product rendering in Home component

--- src/modules/Home/pages/Home.tsx
+++ src/modules/Home/pages/Home.tsx
@@ -9,24 +9,38 @@ import { sliderBreakpoints } from "../constants";
 const Home = () => {
   const { data: response } = useGetProducts();
 
+  const renderProducts = () => {
+    if (!response?.data?.length) {
+      return (
+        <div className="w-full mt-36 flex items-center justify-center">
+          <h1 className="text-xl">
+            Oops! Parece que não há produtos disponíveis.
+          </h1>
+        </div>
+      );
+    }
+
+    return (
+      <Slider
+        slidesToShow={3}
+        nextArrow={<NextArrow />}
+        prevArrow={<PrevArrow />}
+        responsive={sliderBreakpoints}
+      >
+        {response?.data?.map((product) => (
+          <ProductCard key={product.slug} product={product} />
+        ))}
+      </Slider>
+    );
+  };
+
   return (
     <div className="flex flex-col gap-6">
       <div>
         <h1 className="text-2xl font-bold">Produtos</h1>
         <h3 className="text-gray-500">Confira nossos produtos em destaque!</h3>
       </div>
-      <div className="flex flex-col gap-2 p-4 lg:p-0">
-        <Slider
-          slidesToShow={3}
-          nextArrow={<NextArrow />}
-          prevArrow={<PrevArrow />}
-          responsive={sliderBreakpoints}
-        >
-          {response?.data?.map((product) => (
-            <ProductCard key={product.slug} product={product} />
-          ))}
-        </Slider>
-      </div>
+      <div className="flex flex-col gap-2 p-4 lg:p-0">{renderProducts()}</div>
     </div>
   );
 };
